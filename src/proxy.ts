ebee57deb7163674de25cf5aa21ef01bfa88a90e
HoridasPro <horidassarker6@gmail.com>
2026-08-01 13:39:52 +0600

    added to not-found page and role base authorization

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,37 +1,60 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import jwt,{JwtPayload} from "jsonwebtoken"
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 // This function can be marked `async` if using `await` inside
-const AUTH_ROUTES=["/login","/register"]
+const AUTH_ROUTES = ["/login", "/register"];
+const PUBLIC_ROUTES = ["/", "/gears"];
 export function proxy(request: NextRequest) {
-  const pathname=request.nextUrl.pathname
-   
-  const accessToken=request.cookies.get("accessToken")?.value
-  const decodedToken=accessToken?jwt.decode(accessToken) as JwtPayload : null
-  let userRole=null
-  if(decodedToken){
-    userRole=decodedToken.role
+  const pathname = request.nextUrl.pathname;
+
+  const accessToken = request.cookies.get("accessToken")?.value;
+  const decodedToken = accessToken
+    ? (jwt.decode(accessToken) as JwtPayload)
+    : null;
+  let userRole = null;
+  if (decodedToken) {
+    userRole = decodedToken.role;
   }
-  if(accessToken && AUTH_ROUTES.includes(pathname)){
-    if(userRole==="CUSTOMER"){
-      return NextResponse.redirect(new URL("/dashboard",request.url))
-    }else if(userRole==="PROVIDER"){
-return NextResponse.redirect(new URL("/provider-dashboard",request.url))
-    }
-    else if(userRole==="ADMIN"){
-return NextResponse.redirect(new URL("/admin-dashboard",request.url))
-    }
-    else{
-return NextResponse.redirect(new URL("/",request.url))
+  // rolebase access
+  if (accessToken && AUTH_ROUTES.includes(pathname)) {
+    if (userRole === "CUSTOMER") {
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+    } else if (userRole === "PROVIDER") {
+      return NextResponse.redirect(new URL("/provider-dashboard", request.url));
+    } else if (userRole === "ADMIN") {
+      return NextResponse.redirect(new URL("/admin-dashboard", request.url));
+    } else {
+      return NextResponse.redirect(new URL("/", request.url));
     }
   }
-  return NextResponse.next()
+  // authentication
+  const isPublicRoute = PUBLIC_ROUTES.some(
+    (route) => pathname === route || pathname.startsWith(route + "/"),
+  );
+  const isAuthRoute = AUTH_ROUTES.some(
+    (route) => pathname === route || pathname.startsWith(route + "/"),
+  );
+  if (!accessToken && !isPublicRoute && !isAuthRoute) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+  // authorazation
+  if (pathname.startsWith("/dashboard") && userRole !== "CUSTOMER") {
+    return NextResponse.redirect(new URL("/", request.url));
+  } else if (
+    pathname.startsWith("/provider-dashboard") &&
+    userRole !== "PROVIDER"
+  ) {
+    return NextResponse.redirect(new URL("/not-found", request.url));
+  } else if (pathname.startsWith("/admin-dashboard") && userRole !== "ADMIN") {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+  return NextResponse.next();
 }
 
 // Alternatively, you can use a default export:
 // export default function proxy(request: NextRequest) { ... }
 
 export const config = {
-  matcher:  ['/((?!api|_next/static|favicon.ico|_next/image|.*\\.png$).*)',]
+  matcher: ["/((?!api|_next/static|favicon.ico|_next/image|.*\\.png$).*)"],
 };
