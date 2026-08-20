77557261f2d7cc41f387cc9dfb87d7fca451bc38
HoridasPro <horidassarker6@gmail.com>
2026-08-20 11:27:30 +0600

    fix dynamic provider gears page

--- src/app/(dashboardGroup)/provider-dashboard/my-gears/page.tsx
+++ src/app/(dashboardGroup)/provider-dashboard/my-gears/page.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+export const dynamic = "force-dynamic";
 import { Gear, GearResponse } from "@/type/type-gear";
 import { getProviderGears } from "../../_actions/get-provider-gear";
 import Image from "next/image";
